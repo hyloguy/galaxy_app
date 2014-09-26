5f2554c3d16c6c5ae2759f44503a98bb69a57e4b
Michael Rubinstein <michael@polyblog.com>
2014-09-25 21:14:39 -0700

    Implemented index, new, create, and show (for planets)

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -1,13 +1,28 @@
 class PlanetsController < ApplicationController
+
   def index
+  	@planets = Planet.all
   end
 
   def new
   end
 
   def show
+  	@planet = Planet.find(params[:id])
   end
 
   def edit
   end
+
+  def create
+  	@planet = Planet.create(planet_params)
+  	redirect_to planets_path
+  end
+
+  private
+
+  def planet_params
+  	params.require(:planet).permit(:name, :diameter)
+  end
+
 end
