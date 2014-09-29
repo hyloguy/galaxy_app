e7143d15a07a8d2315f65288d89e388d551880cb
Michael Rubinstein <michael@polyblog.com>
2014-09-28 20:34:24 -0700

    Added login stuff I don't understand yet

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,2 +1,23 @@
 class LoginsController < ApplicationController
+
+  # THE CODE BELOW IS ADDED FROM:
+  # http://guides.rubyonrails.org/action_controller_overview.html
+
+  # "Create" a login, aka "log the user in"
+  def create
+    if user = User.authenticate(params[:username], params[:password])
+      # Save the user ID in the session so it can be used in
+      # subsequent requests
+      session[:current_user_id] = user.id
+      redirect_to root_url
+    end
+  end
+
+  # "Delete" a login, aka "log the user out"
+  def destroy
+    # Remove the user id from the session
+    @_current_user = session[:current_user_id] = nil
+    redirect_to root_url
+  end
+  
 end
