b5760968cbc4a6d23b0791975f837dccd56f275d
hyloguy <michael@polyblog.com>
2014-09-30 14:28:22 -0700

    Added flash notices for logout and for successful/unsuccessful logins

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -10,6 +10,10 @@ class LoginsController < ApplicationController
       # Save the user ID in the session so it can be used in
       # subsequent requests
       session[:current_user_id] = @user.id
+      flash[:success] = 'You have been successfully logged in.'
+      redirect_to root_path
+    else
+      flash[:danger] = 'Login Failed!'
       redirect_to root_path
     end
   end
@@ -18,6 +22,7 @@ class LoginsController < ApplicationController
   def destroy
     # Remove the user id from the session
     @_current_user = session[:current_user_id] = nil
+    flash[:success] = 'You have been successfully logged out.'
     redirect_to root_path
   end
   
